ebcdb462f1231e1093b9e300dd809af30f07d5fa
Ashishk141 <akuniverse141@gmail.com>
2025-08-22 10:45:12 +0530

    v41 Styling React Applications

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -62,17 +62,6 @@ function App() {
 // Components
 // function Header
 
-// components
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
-
 // Component
 function Header() {
   // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
@@ -85,15 +74,40 @@ function Header() {
   );
 }
 
-// Component
+// Parent Component
 function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
+  );
+}
+
+// Child components of Menu
+function Pizza(props) {
+  console.log(props);
+
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
   );
 }
 

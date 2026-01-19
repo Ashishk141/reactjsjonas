6d764c5f0558b684d26ca143df110f9a742915d2
Ashishk141 <akuniverse141@gmail.com>
2026-01-19 13:15:06 +0530

    v71 Rendering the items lists

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -22,7 +28,32 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {/* map just iterate over the array and return the JSX */}
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      {/* Conditionally we write styling */}
+      <span
+        style={{
+          textDecoration: item.packed ? "line-through" : "none",
+        }}
+      >
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
